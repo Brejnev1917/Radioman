570bf55c592bd771ab0309c6b12b17b7be2f7db1
brejnev1917 <brejnev1917@gmail.com>
2022-01-19 16:45:14 +0300

    add Radio.java

---
+++ src/main/java/ru/netology/domain/Radio.java
@@ -0,0 +1,102 @@
+package ru.netology.domain;
+
+public class Radio {
+
+    private int maxVolume;
+    private int minVolume;
+    private int currentVolume;
+    private int maxStation;
+    private int minStation;
+    private int currentStation;
+
+    // ГРОМКОСТЬ
+// Максимальная громкость
+    public int getMaxVolume() {
+        return maxVolume;
+    }
+
+    public void setMaxVolume(int maxVolume) {
+        this.maxVolume = 10;
+    }
+
+    // Минимальная  громкость
+    public int getMinVolume() {
+        return minVolume;
+    }
+
+    public void setMinVolume(int minVolume) {
+        this.minVolume = 0;
+    }
+
+    // Текущая громкость
+    public int getCurrentVolume() {
+        return currentVolume;
+    }
+
+    public void setCurrentVolume(int currentVolume) {
+        this.currentVolume = currentVolume;
+    }
+
+    //Увеличение громкости +
+    public void upVolume() {
+        if (currentVolume == maxVolume) {
+            return;
+        }
+        this.currentVolume++;
+    }
+
+    //Уменьшение громкости -
+    public void downVolume() {
+        if (currentVolume == minVolume) {
+            return;
+        }
+        this.currentVolume--;
+    }
+
+    // СТАНЦИИ
+    // Максимальная станция
+    public int getMaxStation() {
+        return maxStation;
+    }
+
+    public void setMaxStation(int maxStation) {
+        this.maxStation = 9;
+    }
+
+    // Минимальная  станция
+    public int getMinStation() {
+        return minStation;
+    }
+
+    public void setMinStation(int minStation) {
+        this.minStation = 0;
+    }
+
+    public int getCurrentStation() {
+        return currentStation;
+    }
+
+    //Увеличение станции
+    public void upStation() {
+        if (currentStation < maxStation) {
+            currentStation++;
+            this.currentStation++;
+            return;
+        }
+        this.currentStation = minStation;
+    }
+
+    //Уменьшение станции
+    public void downStation() {
+        if (currentStation > minStation) {
+            currentStation--;
+            this.currentStation--;
+            return;
+        }
+        this.currentStation = maxStation;
+    }
+
+    //Установка текущей станции
+    public void setCurrentStation(int currentStation) {
+    }
+}
